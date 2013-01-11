98361a0f7b290661fabdcfbe14e4762220eb907b
Esben Sorig <eas@trifork.com>
2013-01-11 15:26:55 +0000

    Fixed turning algorithm

--- 1-3.c
+++ 1-3.c
@@ -5,14 +5,61 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-void turnRobot(int *sock, char *buf, int iterations)
+typedef struct {
+    int leftME;
+    int rightME;
+} MEValue;
+
+MEValue readME(int *sock, char *buf)
 {
-    int speed = 40, i;
-    for (i = 0; i < iterations; i++) {
+    // Send message
+    sprintf(buf, "S MELR\n");
+    write(*sock, buf, strlen(buf));
+    memset(buf, 0, 80);
+    read(*sock, buf, 80);
+    
+    // Parse the response
+    char *secondInt = buf + strlen(buf);
+    while (*secondInt != ' ') {
+        secondInt--;
+    }
+    char *firstInt = secondInt-1;
+    *secondInt = '\0';
+    secondInt++;
+    
+    while (*firstInt != ' ') {
+        firstInt--;
+    }
+    firstInt++;
+    
+    // Construct the return value
+    MEValue returnValue;
+    returnValue.leftME = atoi(firstInt);
+    returnValue.rightME = atoi(secondInt);
+    return returnValue;
+}
+
+double averageMEDifference(MEValue a, MEValue b)
+{
+    int leftDifference = abs(a.leftME - b.leftME);
+    int rightDifference = abs(a.rightME - b.rightME);
+    return (leftDifference + rightDifference)/2;
+}
+
+void turnRobot(int *sock, char *buf, int degrees)
+{
+    MEValue initialME = readME(sock, buf);
+    MEValue currentME = initialME;
+    
+    while (averageMEDifference(initialME, currentME) <= degrees*(2.25))
+    {
+        int speed = 1 * (degrees/abs(degrees));
         sprintf(buf, "M LR %i %i\n", speed, -speed);
         write(*sock, buf, strlen(buf));
         memset(buf, 0, 80);
         read(*sock, buf, 80);
+        
+        currentME = readME(sock, buf);
     }    
 }
 
@@ -21,9 +68,9 @@ void driveRobot(int *sock, char *buf, int length)
     int i;
     for (i = 0; i < length; i++) {
         sprintf(buf, "M LR 50 50\n");
-         write(*sock, buf, strlen(buf));
-         memset(buf, 0, 80);
-         read(*sock, buf, 80);
+        write(*sock, buf, strlen(buf));
+        memset(buf, 0, 80);
+        read(*sock, buf, 80);
         
         sprintf(buf, "C TRAIL\n");
         write(*sock, buf, strlen(buf));
@@ -34,14 +81,14 @@ void driveRobot(int *sock, char *buf, int length)
 
 void drawSquare(int *sock, char *buf)
 {
-    int turningConstant = 34;
     driveRobot(sock, buf, 50);
-    turnRobot(sock, buf, turningConstant);
+    turnRobot(sock, buf, 90);
     driveRobot(sock, buf, 50);
-    turnRobot(sock, buf, turningConstant);
+    turnRobot(sock, buf, 90);
     driveRobot(sock, buf, 50);
-    turnRobot(sock, buf, turningConstant);
-    driveRobot(sock, buf, 50);    
+    turnRobot(sock, buf, 90);
+    driveRobot(sock, buf, 50);
+    
 }
 
 void drawTriangle(int *sock, char *buf)
@@ -79,4 +126,5 @@ int main() {
     //turnRobot(&sock, buf, 90);
     //driveRobot(&sock, buf, 10);
     drawSquare(&sock, buf);
+    //readME(&sock, buf);
 }
