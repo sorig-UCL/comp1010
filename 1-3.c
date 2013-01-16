b5b54a62715fb4a5555c5d2e7b2f57ac3a7f339b
Esben Sorig <eas@trifork.com>
2013-01-16 13:45:02 +0000

    Added menu system. Program now has an interface

--- 1-3.c
+++ 1-3.c
@@ -9,8 +9,8 @@ void drawSquare(int sock, double size)
     for (i = 0; i < 4; i++)
     {
         driveRobot(sock, size, 60, 1.0);
-        if (i < 3) {
-            stopMotorsAndWait(sock, 1);
+        stopMotorsAndWait(sock, 1);
+        if (i < 3) {            
             turnRobot(sock, 90);
         }
     }
@@ -22,8 +22,8 @@ void drawTriangle(int sock, double size)
     for (i = 0; i < 3; i++)
     {
         driveRobot(sock, size, 60, 1.0);
-        if (i < 2) {
-            stopMotorsAndWait(sock, 1);
+        stopMotorsAndWait(sock, 1);
+        if (i < 2) {            
             turnRobot(sock, 120);
         }        
     }    
@@ -35,8 +35,8 @@ void drawStar(int sock, double size)
     for (i = 0; i<5; i++)
     {
         driveRobot(sock, size, 60, 1.0);
-        if (i < 4) {
-            stopMotorsAndWait(sock, 1);
+        stopMotorsAndWait(sock, 1);
+        if (i < 4) {            
             turnRobot(sock, 144);
         }        
     }    
@@ -50,16 +50,57 @@ void drawCircle(int sock, double radius)
     double wheelTurns = circumference*(3.0/4.0);
     
     driveRobot(sock, wheelTurns, 60, ratio);
+    stopMotorsAndWait(sock, 0);
+}
+
+void presentMenu(int sock)
+{
+    printf("What would you like to draw? (type in an integer)\n");
+    printf("1. Square\n");
+    printf("2. Triangle\n");
+    printf("3. Star\n");
+    printf("4. Circle\n");
+    
+    int input;
+    scanf("%i", &input);
+    
+    if (input >= 1 && input <= 3) {
+        printf("Please enter the size of the shape (in wheel turns): \n");
+    }
+    else if(input == 4)
+    {
+        printf("Please enter the radius (in wheel turns): \n");
+    }
+    
+    double size;
+    scanf("%lf", &size);
+    
+    printf("Drawing..\n");
+    
+    switch (input) {
+        case 1:
+            drawSquare(sock, size);
+            break;
+        case 2:
+            drawTriangle(sock, size);
+            break;
+        case 3:
+            drawStar(sock, size);
+            break;
+        case 4:
+            drawCircle(sock, size);
+            break;
+        default:
+            break;
+    }
+    
+    printf("Done!\n\n");
 }
 
 int main() {
 	int sock = connectAndGetSocket();
     
-    //turnRobot(sock, 720);
-    //driveRobot(sock, 10);
-    //drawSquare(sock);
-    drawCircle(sock, 1.0);
-    //drawTriangle(sock);
-    //drawStar(sock, 2.0);
-    //readME(sock);
+    while (1) {
+        presentMenu(sock);
+    }
 }
